f3455f2dc1e74c7e5ea486eb2e029c046372052d
serenapascual <serena.a.pascual@gmail.com>
2020-10-08 17:37:00 -0700

    Add hamburger menu for small screens, animate logo with Lottie

--- js/script.js
+++ js/script.js
@@ -1,10 +1,8 @@
+// Logo stuff
 resize = function() {
-  const logo = $("#logo");
-  const newImage = new Image();
-  newImage.src = logo.attr("src");
-  const imageWidth = 100;
+  const imageWidth = $("#lottie").width();
 
-  // assign distance for resize to take place over
+  // Assign distance for resize to take place over
   let maxScrollDist = 300;
   maxScrollDist = Math.min(maxScrollDist, $(window).height());
   
@@ -16,7 +14,49 @@ resize = function() {
     const scrollPos = Math.min($(document).scrollTop(), maxScrollDist);
     const sumScrollDelta = Math.floor(scrollPos * scrollDelta);
     const newWidth = imageWidth - sumScrollDelta;
-    $('#logo').css('width', newWidth);
+    $('#lottie').css('width', newWidth);
   });
 }
 resize();
+
+let logo = document.getElementById("lottie");
+let direction = -1; // normal play direction
+
+let logoAnimation = bodymovin.loadAnimation({
+  container: logo,
+  path: 'res/data.json',
+  renderer: 'svg',
+  loop: false,
+  autoplay: false,
+  name: "logo",
+});
+logoAnimation.goToAndStop(30, true);
+let visible = true;
+
+logo.addEventListener('click', (e) => {
+  logoAnimation.setDirection(direction);
+  logoAnimation.goToAndPlay(30, true);
+  visible = false;
+});
+
+logo.addEventListener('mouseleave', (e) => {
+  if (!visible) {
+    logoAnimation.setDirection(-direction);
+    logoAnimation.goToAndPlay(0, true);
+    visible = true;
+  }
+});
+
+// Nav stuff
+const navToggle = document.querySelector('.nav-toggle');
+const navLinks = document.querySelectorAll('.nav__link');
+
+navToggle.addEventListener('click', () => {
+  document.body.classList.toggle('nav-open');
+});
+
+navLinks.forEach(link => {
+  link.addEventListener('click', () => {
+    document.body.classList.remove('nav-open');
+  })
+});
